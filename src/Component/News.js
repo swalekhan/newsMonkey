7d6e29bba2267361d4d887061b772cd26f64cbbb
swalekhan <swalekhan1522@gmail.com>
2023-06-11 14:30:43 +0530

    vercel

--- src/Component/News.js
+++ src/Component/News.js
@@ -39,12 +39,12 @@ class News extends Component {
         return (
             <div className="container my-3">
                 <h1 className="text-center" style={{ margin: "35px 0px" }}> {this.cate} headlines</h1>
-                {this.state.loading && <Spiner />}    {/* if loading true then spiner will be shown or else not */}
+                {this.state?.loading && <Spiner />}    {/* if loading true then spiner will be shown or else not */}
                 <div className="row">
-                    {!this.state.loading && this.state.articles.map((e) => {
+                    {!this.state?.loading && this.state?.articles?.map((e) => {
                         // {/* if loading false then content will be shown or else not  */ }
                         return <div className="col-md-4" key={e.url}>
-                            <NewsItem title={e.title ? e.title.slice(0, 40) : ""} discription={e.description ? e.description.slice(0, 40) : ""} imageUrl={e.urlToImage ? e.urlToImage : "https://images.hindustantimes.com/tech/img/2022/12/12/1600x900/bda3ef1394e93d39b36a9aa6305e7c64jpg_1_1670807253601_1670807253842_1670807253842.jpg"} newsUrl={e.url} publishedAt={e.publishedAt} author={e.author ? e.author : "unknow"} source={e.source.name} />
+                            <NewsItem title={e.title ? e.title?.slice(0, 40) : ""} discription={e.description ? e.description.slice(0, 40) : ""} imageUrl={e.urlToImage ? e.urlToImage : "https://images.hindustantimes.com/tech/img/2022/12/12/1600x900/bda3ef1394e93d39b36a9aa6305e7c64jpg_1_1670807253601_1670807253842_1670807253842.jpg"} newsUrl={e.url} publishedAt={e.publishedAt} author={e.author ? e.author : "unknow"} source={e.source.name} />
                         </div>
                     })}
                     <div className="container d-flex justify-content-between ">
